5f19c5cef9ce8a58b6e43ce4c0c20d9c150bfe22
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-29 21:03:33 +0600

    navbar icon setting done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Navbar from './components/sections/Navbar'
+import Navbar from './components/sections/Navbar';
+
 
 
 function App() {
   return (
-    <section>
+    <section className='bg-neutral-200 rounded-lg'>
       <Navbar />
     </section>
   );
--- src/components/sections/Navbar.js
+++ src/components/sections/Navbar.js
@@ -1,10 +1,49 @@
 import React from 'react';
+const icon_stroke = 'lightgrey';
+const home = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke={icon_stroke} strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+</svg>
+const search = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke={icon_stroke} strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+</svg>
+const bell = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke={icon_stroke} strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+</svg>
+const logout = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke={icon_stroke} strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+</svg>
+const table = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke={icon_stroke} strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+</svg>
+const user = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke={icon_stroke} strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+</svg>
+const newspaper = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke={icon_stroke} strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
+</svg>
+
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>nav</h1>
-        </div>
+        <nav className='px-5 w-16 bg-white'>
+            <ul className='flex flex-col justify-around h-screen'>
+                <div className=''>
+                    <li className='pb-3'>logo </li>
+                    <li className='pb-3'>{search} </li>
+                </div>
+                <div className=''>
+                    <li className='pb-3'>{home}</li>
+                    <li className='pb-3'>{table} </li>
+                    <li className='pb-3'>{newspaper}</li>
+                    <li className='pb-3'>{user} </li>
+                </div>
+                <div className=''>
+                    <li className='pb-3'>{bell} </li>
+                    <li className='pb-3'>{logout} </li>
+
+                </div>
+            </ul>
+        </nav>
     );
 };
 
