8e3201d90589ba3becaead10ab1e2608974f9614
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-29 21:25:51 +0600

    user img done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import HelloSection from './components/sections/Hello_section/HelloSection';
 import Navbar from './components/sections/Navbar';
 
 
 
 function App() {
   return (
-    <section className='bg-neutral-200 rounded-lg'>
+    <section className='bg-neutral-100 rounded-lg grid grid-cols-4'>
       <Navbar />
+      <HelloSection />
     </section>
   );
 }
--- src/components/sections/Hello_section/HelloSection.js
+++ src/components/sections/Hello_section/HelloSection.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import user_pic from '../../../../src/user-pic.jpg'
 
 const HelloSection = () => {
     return (
-        <div>
-            <h1>Hello Mike</h1>
-        </div>
+        <section className='flex flex-col items-center mt-10'>
+            <div className='flex justify-around items-center w-2/3'>
+                <div>
+                    <img className='inline-block h-20 w-20 rounded-full ring-2 ring-white' src={user_pic} alt='' />
+                </div>
+                <div>
+                    <h3 className='text-3xl font-bold'>Hi Mike,</h3>
+                    <p>welcome back</p>
+                </div>
+            </div>
+
+        </section>
     );
 };
 
